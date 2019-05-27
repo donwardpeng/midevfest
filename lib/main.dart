173aef8dce3cabe1b79eb42b7d2e3f2fb40317a4
Don Ward <don.ward.peng@gmail.com>
2019-05-26 22:09:42 -0400

    Dived into creating a string resource file and trying to get localization working - tried one library - https://pub.dev/packages/gen_lang#-readme-tab- but it does not seem to work with Flutter for Web yet. I ended up rolling a a json file with strings that I decode and use per page in the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter_web/material.dart';
 import 'theme.dart';
 import 'flutter_swiper.dart';
@@ -20,15 +22,35 @@ class MyHomePage extends StatefulWidget {
   final String title;
   MyHomePage({Key key, this.title}) : super(key: key);
 
- @override
+  @override
   State<StatefulWidget> createState() {
     return HomePageState();
   }
 }
-class HomePageState extends State<MyHomePage>{
 
-  List<String> imagesList = ['devfest2018_photo_1.jpg','devfest2018_photo_2.jpg', 'devfest2018_photo_3.jpg' ];
+class HomePageState extends State<MyHomePage> {
+  List<String> imagesList = [
+    'devfest2018_photo_1.jpg',
+    'devfest2018_photo_2.jpg',
+    'devfest2018_photo_3.jpg'
+  ];
   // Map fi;
+
+  Map<String, dynamic> stringResources;
+
+
+  void readStrings() async {
+    String data = await DefaultAssetBundle.of(context).loadString("main_page_string_en.json");
+    print("Here is the data read in" + data);
+    stringResources = json.decode(data);
+    print("Here is the value in String Resources = " + stringResources['devFestName']);
+  }
+
+  @override
+  void initState() {
+    readStrings();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,61 +78,72 @@ class HomePageState extends State<MyHomePage>{
           ],
         ),
         body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-              Padding(padding: EdgeInsets.fromLTRB(0, 5, 0, 5)),
-               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                      width: 600,
-                      height: 300,
-                      child: Card(child: Image.asset('midevfest_logo.png'), elevation: 24,)),
-              Expanded(
-              flex: 4,
-              child: 
-              Swiper(
-                  autoplay: true,
-                  // outer: true,
-                  layout: SwiperLayout.TINDER,
-                  itemHeight: 350,
-                  itemWidth: 350,
-                  // scrollDirection: Axis.vertical,
-                 // onIndexChanged: (n) => setState(() => fi = imagesList[n]),
-                  itemCount: imagesList.length,
-                  itemBuilder: (cx, i) {
-                    return Container(
-                        margin: EdgeInsets.only(top: 8, bottom: 8),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child:
-                              Image.asset(imagesList[i], fit: BoxFit.cover)),
-                        );
-                  },
-                  viewportFraction: .85,
-                  scale: .9)
-                  )
-                  ,]),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                      width: 600,
-                      height: 300,
-                      child: Card(child: Image.asset('midevfest_logo.png'), elevation: 24,)),
-                  SizedBox(
-                    width: 500,
-                    height: 300,
-                    child: Card(
-                      elevation: 24,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: <
+                        Widget>[
+          Padding(
+              padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 4,
+                      child: SizedBox(
+                          width: 500,
+                          height: 250,
+                          child: Card(
+                            child: Padding(
+                                padding: EdgeInsets.all(15),
+                                child: Image.asset('midevfest_logo.png')),
+                            elevation: 24,
+                          )),
+                    ),
+                    Expanded(
+                        flex: 2,
+                        child: Swiper(
+                            autoplay: true,
+                            // outer: true,
+                            layout: SwiperLayout.TINDER,
+                            itemHeight: 325,
+                            itemWidth: 325,
+                            // scrollDirection: Axis.vertical,
+                            // onIndexChanged: (n) => setState(() => fi = imagesList[n]),
+                            itemCount: imagesList.length,
+                            itemBuilder: (cx, i) {
+                              return Container(
+                                margin: EdgeInsets.only(top: 8, bottom: 8),
+                                child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10),
+                                    child: Image.asset(imagesList[i],
+                                        fit: BoxFit.cover)),
+                              );
+                            },
+                            viewportFraction: .85,
+                            scale: .9)),
+                  ])),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                  width: 600,
+                  height: 300,
+                  child: Card(
+                    child: Image.asset('midevfest_logo.png'),
+                    elevation: 24,
+                  )),
+              SizedBox(
+                width: 500,
+                height: 300,
+                child: Card(
+                    elevation: 24,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
+            
                         Text(
-                          'More Details Coming Soon!',
-                          style: Theme.of(context).textTheme.subtitle,
+                            'test',
+                            style: Theme.of(context).textTheme.subtitle,
                         ),
                         // Text(
                         //   '',
@@ -118,10 +151,10 @@ class HomePageState extends State<MyHomePage>{
                         // )
                       ],
                     )),
-                  ),
-                ],
-              )
-            ] // This trailing comma makes auto-formatting nicer for build methods.
-                )));
+              ),
+            ],
+          )
+        ] // This trailing comma makes auto-formatting nicer for build methods.
+                    )));
   }
 }
