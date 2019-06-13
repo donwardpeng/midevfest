af96514cbe9a41fe4430c4372eb29736991828ff
Don Ward <don.ward.peng@gmail.com>
2019-06-12 22:09:06 -0400

    Explored various possibilities to get Cloud Firestore working in Flutter for Web. Since the Flutter plugin system is not yet working for Flutter for Web, I can not use the Flutter Firebase plugin. I tried using the dart:js library but it seemed to tedious. Trying out the Dart Firebase project now, and I am pretty close to use it. If this works, I have yet to see any other examples or codebases using this technique as an interim measure.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import './config_values/en_strings.dart';
 import './ui/mainpage.dart';
 import './ui/teampage.dart';
 import './widgets/state_widget.dart';
+import 'package:firebase/firebase.dart';
+import 'package:firebase/firestore.dart' as fs;
+
+external void log(dynamic str);
 
 void main() => runApp(new StateWidget(child: MyApp()));
 
@@ -11,6 +15,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // log('Hello MIDevFest!');    
+    
     return MaterialApp(
       title: EN_Strings.devFestName,
       theme: buildTheme(),
