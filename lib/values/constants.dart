9cf1d3027270ca581823bc3e662ed9e8fe34c42c
donwardpeng <don.ward.peng@gmail.com>
2019-06-24 12:11:44 -0400

    Changed the locations of the Teams and Partners Json files to Firebase Storage. Needed to open up CORS in Firebase to allow this to happen.

--- lib/values/constants.dart
+++ lib/values/constants.dart
@@ -1,5 +1,8 @@
 class Constants{
-  // static String TEAM_DATA_URL = 'https://firebasestorage.googleapis.com/v0/b/midevfest.appspot.com/o/team.json?alt=media&token=a38ddead-7b2e-407b-8b70-6b2e1d88c7e8';
-  static String TEAM_DATA_URL = 'assets/data/team.json';
-  static String SPONSOR_DATA_URL = 'assets/data/partners.json';
+  static String TEAM_DATA_URL = 'https://firebasestorage.googleapis.com/v0/b/midevfest.appspot.com/o/team.json?alt=media&token=a38ddead-7b2e-407b-8b70-6b2e1d88c7e8';
+  static String SPONSOR_DATA_URL = 'https://firebasestorage.googleapis.com/v0/b/midevfest.appspot.com/o/partners.json?alt=media&token=d83c4076-f652-4976-b490-6bbb10481105';
+
+  // static String TEAM_DATA_URL = 'assets/data/team.json';
+  // static String SPONSOR_DATA_URL = 'assets/data/partners.json';
+
 }
