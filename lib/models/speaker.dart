0cea09ab5e625008910ad3811bb6372986344f85
donwardpeng <don.ward.peng@gmail.com>
2019-07-04 22:39:41 -0400

    Cleaned up Json for speakers and sessions, and halfway complete with the classes for json_serializable to serialize the Json.

---
+++ lib/models/speaker.dart
@@ -0,0 +1,62 @@
+import 'package:json_annotation/json_annotation.dart';
+import './social.dart';
+
+part 'speaker.g.dart';
+
+@JsonSerializable()
+class Speaker {
+  Speaker(
+      {this.id,
+      this.featured,
+      this.name,
+      this.title,
+      this.company,
+      this.companyLogo,
+      this.country,
+      this.photoUrl,
+      this.shortBio,
+      this.bio,
+      this.tags,
+      this.badges,
+      this.socials});
+  final String id;
+  final bool featured;
+  final String name;
+  final String title;
+  final String company;
+  final String companyLogo;
+  final String country;
+  final String photoUrl;
+  final String shortBio;
+  final String bio;
+  final List<String> tags;
+  final List<String> badges;
+  final List<Social> socials;
+
+  factory Speaker.fromJson(Map<String, dynamic> json) => _$SpeakerFromJson(json);
+  Map<String, dynamic> toJson() => _$SpeakerToJson(this);
+}
+
+// "title": "Speakers",
+//   "listOfSpeakers": [
+//     {
+//       "id": 1,
+//       "featured": true,
+//       "name": "Jason Rotole",
+//       "title": "",
+//       "company": "Google",
+//       "companyLogo": "/images/logos/google.svg",
+//       "country": "Detroit, Michigan, USA",
+//       "photoUrl": "/images/people/jasonrotole.jpg",
+//       "shortBio": "Jason Rotole has over 20 years of systems engineering and technical architecture experience...",
+//       "bio": "Jason Rotole has over 20 years of systems engineering and technical architecture experience with a focus on data and analytics solutions in a private, hybrid and public cloud.  In his past, Jason has worked with large multinational organizations to solution and implement BigData solutions aimed at solving supply chain, customer and engineering challenges.  Jason currently works at Google helping organizations in the Great Lakes region to solve business problems using Google's cloud and analytic solutions.",
+//       "tags": ["Big Query", "Machine Learning"],
+//       "badges": [],
+//       "socials": [
+//         {
+//           "icon": "linkedin",
+//           "name": "LinkedIn",
+//           "link": "https://www.linkedin.com/in/jasonrotole/"
+//         }
+//       ]
+//     },
---
+++ lib/models/speaker.g.dart
@@ -0,0 +1,43 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'speaker.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Speaker _$SpeakerFromJson(Map<String, dynamic> json) {
+  return Speaker(
+      id: json['id'] as String,
+      featured: json['featured'] as bool,
+      name: json['name'] as String,
+      title: json['title'] as String,
+      company: json['company'] as String,
+      companyLogo: json['companyLogo'] as String,
+      country: json['country'] as String,
+      photoUrl: json['photoUrl'] as String,
+      shortBio: json['shortBio'] as String,
+      bio: json['bio'] as String,
+      tags: (json['tags'] as List)?.map((e) => e as String)?.toList(),
+      badges: (json['badges'] as List)?.map((e) => e as String)?.toList(),
+      socials: (json['socials'] as List)
+          ?.map((e) =>
+              e == null ? null : Social.fromJson(e as Map<String, dynamic>))
+          ?.toList());
+}
+
+Map<String, dynamic> _$SpeakerToJson(Speaker instance) => <String, dynamic>{
+      'id': instance.id,
+      'featured': instance.featured,
+      'name': instance.name,
+      'title': instance.title,
+      'company': instance.company,
+      'companyLogo': instance.companyLogo,
+      'country': instance.country,
+      'photoUrl': instance.photoUrl,
+      'shortBio': instance.shortBio,
+      'bio': instance.bio,
+      'tags': instance.tags,
+      'badges': instance.badges,
+      'socials': instance.socials
+    };
