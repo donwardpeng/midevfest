bd6b647e5f2dda598de2e095d0fdbd97a8b0160b
donwardpeng <don.ward.peng@gmail.com>
2019-08-20 07:49:57 -0400

    Made the sponsors page more responsive for large screen layouts.

--- lib/ui/large_layout/sponsors_page_layout.dart
+++ lib/ui/large_layout/sponsors_page_layout.dart
@@ -11,15 +11,19 @@ class SponsorsPageLargeBodyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     appState = StateWidget.of(context).state;
+    var size = MediaQuery.of(context).size;
+    final double height = size.height;
+    final double width = size.width;
+
     print('Building Large Body Widget');
     print(
         'Results for StateWidget SponsorsList - ' + appState.sponsorList.title);
     return Stack(children: <Widget>[
       Padding(
           padding: EdgeInsets.fromLTRB(24, 8, 8, 16),
-          child: Text('Sponsors', style: Theme.of(context).textTheme.headline)),
+          child: Text('Thank you to our sponsors!', style: Theme.of(context).textTheme.headline)),
       Padding(
-          padding: EdgeInsets.fromLTRB(8, 72, 8, 24),
+          padding: EdgeInsets.fromLTRB(8, 84, 8, 24),
           child: GridView.count(
               crossAxisCount: 3,
               children: appState.sponsorList.levels[0].logos
@@ -28,23 +32,27 @@ class SponsorsPageLargeBodyWidget extends StatelessWidget {
                         window.open(sponsor.url, sponsor.name);
                       },
                       child: Padding(
+                
                         padding: EdgeInsets.all(16),
                         child: Card(
+                          
                           elevation: 4,
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
                               Image.network(
                                 FirebaseCloudStorageURLResolver().getCloudStorageURL(Constants.DEVFEST_BUCKET, sponsor.logoUrl),
-                                width: sponsor.width.roundToDouble(),
-                                height: sponsor.height.roundToDouble(),
+                                // width: sponsor.width.roundToDouble(),
+                                // height: sponsor.height.roundToDouble(),
+                                width: double.infinity,
+                                height: height/3,
                                 alignment: Alignment.center,
                               ),
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(0, 32, 0, 0),
-                                child: Text(sponsor.name,
-                                    style: Theme.of(context).textTheme.body1),
-                              )
+                              // Padding(
+                              //   padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+                              //   child: Text(sponsor.name,
+                              //       style: Theme.of(context).textTheme.title),
+                              // )
                             ],
                           ),
                         ),
