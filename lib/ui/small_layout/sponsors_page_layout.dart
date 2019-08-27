b2bc7dd3e8d05c49b6eb119b525361545aede4a1
donwardpeng <don.ward.peng@gmail.com>
2019-08-27 16:12:59 -0400

    Fixed scroll on Sponsors Page on Mobile

--- lib/ui/small_layout/sponsors_page_layout.dart
+++ lib/ui/small_layout/sponsors_page_layout.dart
@@ -7,44 +7,44 @@ import 'dart:html';
 import '../../helper/firebase_cloud_storage_resolver.dart';
 import '../../values/constants.dart';
 
-
 class SponsorsPageSmallBodyWidget extends StatelessWidget {
   StateModel appState;
 
   @override
   Widget build(BuildContext context) {
     appState = StateWidget.of(context).state;
-        var size = MediaQuery.of(context).size;
+    var size = MediaQuery.of(context).size;
     final double height = size.height;
     final double width = size.width;
 
     //populate list of financial sponsors
     List<Sponsor> financialSponsors = List<Sponsor>();
-    for(SponsorLevel level in appState.sponsorList.levels) {
-      if(level.title.contains('Financial')){
-        for(Sponsor sponsor in level.logos){
-        financialSponsors.add(sponsor);
+    for (SponsorLevel level in appState.sponsorList.levels) {
+      if (level.title.contains('Financial')) {
+        for (Sponsor sponsor in level.logos) {
+          financialSponsors.add(sponsor);
         }
       }
     }
     //populate list of financial sponsors
     List<Sponsor> communityPartners = List<Sponsor>();
-    for(SponsorLevel level in appState.sponsorList.levels) {
-      if(level.title.contains('Community')){
-        for(Sponsor partner in level.logos){
-        communityPartners.add(partner);
+    for (SponsorLevel level in appState.sponsorList.levels) {
+      if (level.title.contains('Community')) {
+        for (Sponsor partner in level.logos) {
+          communityPartners.add(partner);
         }
       }
     }
 
-return Scrollbar(child:ListView(children: <Widget>[
+    return Scrollbar(
+        child: ListView(children: <Widget>[
       Padding(
           padding: EdgeInsets.fromLTRB(24, 16, 8, 16),
-          child: Text('Thank you to our sponsors!', style: Theme.of(context).textTheme.display1)),
+          child: Text('Thank you to our sponsors!',
+              style: Theme.of(context).textTheme.display1)),
       Padding(
-          padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
-          child:
-      GridView.count(
+          padding: EdgeInsets.fromLTRB(96, 8, 96, 0),
+          child: GridView.count(
               crossAxisCount: 1,
               shrinkWrap: true,
               children: financialSponsors
@@ -53,16 +53,18 @@ return Scrollbar(child:ListView(children: <Widget>[
                         window.open(sponsor.url, sponsor.name);
                       },
                       child: Padding(
-                        padding: EdgeInsets.all(16),
+                        padding: EdgeInsets.fromLTRB(0, 8, 0, 0),
                         child: Card(
-                          elevation: 4,
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
                               Image.network(
-                                FirebaseCloudStorageURLResolver().getCloudStorageURL(Constants.DEVFEST_BUCKET, sponsor.logoUrl),
+                                FirebaseCloudStorageURLResolver()
+                                    .getCloudStorageURL(
+                                        Constants.DEVFEST_BUCKET,
+                                        sponsor.logoUrl),
                                 width: double.infinity,
-                                height: height/3,
+                                height: height / 3,
                                 alignment: Alignment.center,
                               ),
                             ],
@@ -70,13 +72,13 @@ return Scrollbar(child:ListView(children: <Widget>[
                         ),
                       )))
                   .toList())),
-        Padding(
+      Padding(
           padding: EdgeInsets.fromLTRB(24, 16, 8, 16),
-          child: Text('Thank you to our community partners!', style: Theme.of(context).textTheme.display1)),
-       Padding(
-          padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
-          child:
-      GridView.count(
+          child: Text('Thank you to our community partners!',
+              style: Theme.of(context).textTheme.display1)),
+      Padding(
+          padding: EdgeInsets.fromLTRB(96, 8, 96, 0),
+          child: GridView.count(
               crossAxisCount: 1,
               shrinkWrap: true,
               children: communityPartners
@@ -85,16 +87,18 @@ return Scrollbar(child:ListView(children: <Widget>[
                         window.open(sponsor.url, sponsor.name);
                       },
                       child: Padding(
-                        padding: EdgeInsets.all(16),
+                        padding: EdgeInsets.fromLTRB(0, 8, 0, 0),
                         child: Card(
-                          elevation: 4,
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
                               Image.network(
-                                FirebaseCloudStorageURLResolver().getCloudStorageURL(Constants.DEVFEST_BUCKET, sponsor.logoUrl),
+                                FirebaseCloudStorageURLResolver()
+                                    .getCloudStorageURL(
+                                        Constants.DEVFEST_BUCKET,
+                                        sponsor.logoUrl),
                                 width: double.infinity,
-                                height: height/3,
+                                height: height / 3,
                                 alignment: Alignment.center,
                               ),
                             ],
@@ -104,5 +108,4 @@ return Scrollbar(child:ListView(children: <Widget>[
                   .toList())),
     ]));
   }
-
 }
